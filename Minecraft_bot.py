b81809c1d1ffff87c7bc53510c5199b0ff48ec4d
Slothz <darius4562582@gmail.com>
2025-06-19 17:29:45 +0700

    Update ping

--- Minecraft_bot.py
+++ Minecraft_bot.py
@@ -195,4 +195,21 @@ async def status(interaction: discord.Interaction):
 
 
 
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    Thread(target=run).start()
+keep_alive()
+
 client.run(TOKEN)
